5c342573061cfc733d194d5b13ffcbdc93ddfac2
sanchitss7 <41237387+sanchitss7@users.noreply.github.com>
2019-06-03 15:30:41 +0530

    checkout page created

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Cinzel&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="css/jquery.exzoom.css" rel="stylesheet" />
     <title>Mukesh Jewellers</title>
---
+++ quickview.php
@@ -0,0 +1,16 @@
+<?php
+require_once "db.php";
+require_once "header.php";
+?>
+<div class="container-fluid details-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-xl-10">
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    require_once "footer.php";
+?>
